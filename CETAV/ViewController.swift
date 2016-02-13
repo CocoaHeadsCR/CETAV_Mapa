833eefb5d8f69867c60cf53deacf124734b4928a
Esteban Torres <me@estebantorr.es>
2016-02-12 20:55:10 -0600

    Primer commit

---
+++ CETAV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CETAV
+//
+//  Created by Esteban Torres on 12/2/16.
+//  Copyright © 2016 Esteban Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
