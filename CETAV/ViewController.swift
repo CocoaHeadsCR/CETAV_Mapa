48f14d6196ff26cf493b9e1255aa4cf67ad66702
Esteban Torres <me@estebantorr.es>
2016-02-12 21:35:00 -0600

    Centrar el map en la ubicación del usuario

--- CETAV/ViewController.swift
+++ CETAV/ViewController.swift
@@ -6,14 +6,25 @@
 //  Copyright © 2016 Esteban Torres. All rights reserved.
 //
 
+//// Native Frameworks
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
   // MARK: Propiedades de UI
+  @IBOutlet weak var mapa: MKMapView!
+  
+  // MARK: Propiedades
+  let manager = CLLocationManager()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // Tenemos que solicitar al usuario permiso para acceder a su ubicación
+    manager.requestWhenInUseAuthorization()
+    self.mapa.showsUserLocation = true
+    self.mapa.centerCoordinate = self.mapa.userLocation.coordinate
   }
 }
 
